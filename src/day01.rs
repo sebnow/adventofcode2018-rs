dd159e9abf63e74ea4e20d69bd2e75c52ca4bd83
Sebastian Nowicki <sebnow@gmail.com>
2018-12-01 21:57:57 +0100

    day01: Parse as signed numbers and sum

--- src/day01.rs
+++ src/day01.rs
@@ -1,31 +1,25 @@
 use std::collections::HashSet;
 
-fn process(cur: i32, input: &str) -> i32 {
-    let (sign, num_str) = input.split_at(1);
-    let num = num_str.parse::<i32>().unwrap();
-
-    match sign {
-        "+" => cur + num,
-        "-" => cur - num,
-        _ => panic!("unknown sign"),
-    }
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<i32> {
+    input.lines().map(|l| l.parse::<i32>().unwrap()).collect()
 }
 
 #[aoc(day1, part1)]
-fn answer_1(input: &str) -> i32 {
-    input.lines().fold(0, process)
+fn answer_1(input: &[i32]) -> i32 {
+    input.iter().sum()
 }
 
 #[aoc(day1, part2)]
-fn answer_2(input: &str) -> i32 {
+fn answer_2(input: &[i32]) -> i32 {
     let mut history = HashSet::new();
     let mut cur = 0;
 
     history.insert(cur);
 
     loop {
-        for l in input.lines() {
-            cur = process(cur, l);
+        for i in input.iter() {
+            cur += i;
 
             if history.contains(&cur) {
                 return cur;
@@ -42,16 +36,16 @@ mod test {
 
     #[test]
     fn examples_1() {
-        assert_eq!(3, answer_1("+1\n+1\n+1"));
-        assert_eq!(0, answer_1("+1\n+1\n-2"));
-        assert_eq!(-6, answer_1("-1\n-2\n-3"));
+        assert_eq!(3, answer_1(&vec!(1, 1, 1)));
+        assert_eq!(0, answer_1(&vec!(1, 1, -2)));
+        assert_eq!(-6, answer_1(&vec!(-1, -2, -3)));
     }
 
     #[test]
     fn examples_2() {
-        assert_eq!(0, answer_2("+1\n-1"));
-        assert_eq!(10, answer_2("+3\n+3\n+4\n-2\n-4"));
-        assert_eq!(5, answer_2("-6\n+3\n+8\n+5\n-6"));
-        assert_eq!(14, answer_2("+7\n+7\n-2\n-7\n-4"));
+        assert_eq!(0, answer_2(&vec!(1, -2)));
+        assert_eq!(10, answer_2(&vec!(3, 3, 4, -2, -4)));
+        assert_eq!(5, answer_2(&vec!(-6, 3, 8, 5, -6)));
+        assert_eq!(14, answer_2(&vec!(7, 7, -2, -7, -4)));
     }
 }
