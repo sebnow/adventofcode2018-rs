d70b0e39ba7b1b6c86e02b7ecae527e3db1cc8ea
Sebastian Nowicki <sebnow@gmail.com>
2018-12-02 10:50:55 +0100

    day02: Solve part 2

--- src/day02.rs
+++ src/day02.rs
@@ -32,9 +32,31 @@ fn answer_1(input: &str) -> i32 {
     twice * thrice
 }
 
+fn hamming(a: &str, b: &str) -> String {
+    a.chars()
+        .zip(b.chars())
+        .filter(|(a, b)| a == b)
+        .map(|(a, _)| a)
+        .collect()
+}
+
 #[aoc(day2, part2)]
-fn answer_2(input: &str) -> i32 {
-    0
+fn answer_2(input: &str) -> String {
+    let words: Vec<&str> = input.lines().collect();
+
+    for a in words.iter() {
+        for b in words.iter() {
+            if a == b {
+                continue;
+            }
+            let same = hamming(a, b);
+            if same.len() + 1 == a.len() {
+                return same;
+            }
+        }
+    }
+
+    String::new()
 }
 
 #[cfg(test)]
@@ -57,5 +79,8 @@ mod test {
     }
 
     #[test]
-    fn examples_2() {}
+    fn examples_2() {
+        let input = "abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz";
+        assert_eq!(answer_2(input), "fgij");
+    }
 }
