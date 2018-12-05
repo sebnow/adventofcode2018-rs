1368d4f701bb7a2895cb1ef7a2b5d2bc53525374
Sebastian Nowicki <sebnow@gmail.com>
2018-12-05 18:12:38 +0100

    day05: Solve part 2

--- src/day05.rs
+++ src/day05.rs
@@ -1,7 +1,6 @@
 fn iterate(units: &str) -> String {
     (b'a'..b'z' + 1)
         .map(|c| c as char)
-        .filter(|c| c.is_alphabetic())
         .fold(units.to_owned(), |polymer, c| {
             polymer
                 .replace(&format!("{}{}", c, c.to_uppercase()), "")
@@ -9,16 +8,34 @@ fn iterate(units: &str) -> String {
         })
 }
 
-#[aoc(day5, part1)]
-fn answer_1(units: &str) -> usize {
+fn polymer(units: &str) -> String {
     let mut polymer = units.trim().to_owned();
     loop {
         let old = polymer.len();
         polymer = iterate(&polymer);
         if old == polymer.len() {
-            return polymer.len();
+            return polymer;
         }
     }
+}
+
+#[aoc(day5, part1)]
+fn answer_1(units: &str) -> usize {
+    polymer(units).len()
+}
+
+#[aoc(day5, part2)]
+fn answer_2(units: &str) -> usize {
+    (b'a'..b'z' + 1)
+        .map(|c| c as char)
+        .map(|c| {
+            let x = units
+                .to_owned()
+                .replace(c, "")
+                .replace(c.to_uppercase().next().unwrap(), "");
+            polymer(&x).len()
+        }).min()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -30,33 +47,10 @@ mod test {
         let input = "dabAcCaCBAcCcaDA";
         assert_eq!(answer_1(input), 10);
     }
-    //#[test]
-    //fn examples_1() {
-    //    let input = "dabAcCaCBAcCcaDA";
-    //    assert_eq!(answer_1(&input_generator(input)), 10);
 
-    //    let input = "dabAcCaCBAcCcaA";
-    //    assert_eq!(answer_1(&input_generator(input)), 7);
-
-    //    let input = "AabAcCaCBAcCcaDA";
-    //    assert_eq!(answer_1(&input_generator(input)), 8);
-
-    //    let input = "aA";
-    //    assert_eq!(answer_1(&input_generator(input)), 0);
-
-    //    let input = "abBA";
-    //    assert_eq!(answer_1(&input_generator(input)), 0);
-
-    //    let input = "abAB";
-    //    assert_eq!(answer_1(&input_generator(input)), 4);
-
-    //    let input = "aabAAB";
-    //    assert_eq!(answer_1(&input_generator(input)), 6);
-    //}
-
-    //    #[test]
-    //    fn examples_2() {
-    //        let input = "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2\n";
-    //        assert_eq!(answer_2(&input_generator(input)), 3);
-    //    }
+    #[test]
+    fn examples_2() {
+        let input = "dabAcCaCBAcCcaDA";
+        assert_eq!(answer_2(input), 4);
+    }
 }
