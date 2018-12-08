817eb7789f79abc7c4fdaaff7f6c67293543f753
Sebastian Nowicki <sebnow@gmail.com>
2018-12-08 10:25:42 +0100

    day06: Solve part 1

---
+++ src/day06.rs
@@ -0,0 +1,119 @@
+use point::Point;
+use std::collections::HashMap;
+
+type Grid<'a> = HashMap<Point, (&'a Point, u64)>;
+
+#[aoc_generator(day6)]
+pub fn input_generator(input: &str) -> Vec<Point> {
+    input
+        .lines()
+        .map(|l| {
+            let mut parts = l.split(", ");
+            let x = parts.next().unwrap().parse().unwrap();
+            let y = parts.next().unwrap().parse().unwrap();
+
+            Point::new(x, y)
+        }).collect()
+}
+
+fn bounds(points: &[Point]) -> (Point, Point) {
+    let min = Point::new(0, 0);
+    let max = points.iter().fold(min.clone(), |m, p| {
+        Point::new(
+            if p.x() > m.x() { p.x() } else { m.x() },
+            if p.y() > m.y() { p.y() } else { m.y() },
+        )
+    });
+
+    (min, max)
+}
+
+#[aoc(day6, part1)]
+fn answer_1(input: &[Point]) -> usize {
+    let boundary = bounds(input);
+    let grid = populate_grid(input, &boundary);
+
+    input
+        .iter()
+        .filter(|p| is_finite(&grid, &boundary, p))
+        .map(|p| grid.iter().filter(|(_, (x, _))| p == *x).count())
+        .max()
+        .unwrap()
+}
+
+fn populate_grid<'a>(points: &'a [Point], boundary: &(Point, Point)) -> Grid<'a> {
+    let mut grid: Grid = HashMap::with_capacity(boundary.1.x() as usize * boundary.1.y() as usize);
+    for x in 0..=boundary.1.x() {
+        for y in 0..=boundary.1.y() {
+            let c = Point::new(x, y);
+            let mut closest = points
+                .iter()
+                .map(|p| (p, c.manhattan_distance(p)))
+                .collect::<Vec<(&Point, u64)>>();
+            closest.sort_by(|(_, a), (_, b)| a.cmp(b));
+
+            if closest[0].1 != closest[1].1 {
+                grid.insert(c, closest[0]);
+            }
+        }
+    }
+
+    grid
+}
+
+fn is_finite(grid: &Grid, boundary: &(Point, Point), p: &Point) -> bool {
+    if in_points(grid, &p, (0..=boundary.1.x()).map(|x| Point::new(x, 0))) {
+        return false;
+    }
+    if in_points(
+        &grid,
+        &p,
+        (0..=boundary.1.x()).map(|x| Point::new(x, boundary.1.y())),
+    ) {
+        return false;
+    }
+    if in_points(grid, &p, (0..=boundary.1.y()).map(|y| Point::new(0, y))) {
+        return false;
+    }
+    !in_points(
+        grid,
+        &p,
+        (0..=boundary.1.y()).map(|y| Point::new(boundary.1.x(), y)),
+    )
+}
+
+fn in_points<I>(grid: &Grid, p: &Point, points: I) -> bool
+where
+    I: IntoIterator<Item = Point>,
+{
+    for x in points.into_iter() {
+        if let Some(g) = grid.get(&x) {
+            if g.0 == p {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn examples_1() {
+        assert_eq!(
+            answer_1(&input_generator(
+                "\
+1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9"
+            )),
+            17
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,11 @@ extern crate regex;
 #[macro_use]
 extern crate aoc_runner_derive;
 
+pub mod point;
+
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day06;
 
 aoc_lib!{ year = 2018 }
