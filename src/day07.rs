fdba0c04445849efaac41c64b58f587cc70871c7
Sebastian Nowicki <sebnow@gmail.com>
2018-12-08 17:34:08 +0100

    day07: Solve part 2

--- src/day07.rs
+++ src/day07.rs
@@ -1,19 +1,33 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
 
-#[aoc_generator(day7)]
-pub fn input_generator(input: &str) -> Vec<(char, char)> {
-    input
-        .trim()
-        .lines()
-        .map(|l| {
-            let mut parts = l.split(' ');
+#[derive(Debug, Clone)]
+struct Work(usize, char);
 
-            (
-                parts.nth(1).unwrap().chars().next().unwrap(),
-                parts.nth(5).unwrap().chars().next().unwrap(),
-            )
-        }).collect()
+impl Work {
+    pub fn is_finished(&self, time: usize) -> bool {
+        self.0 + time_required(self.1) < time
+    }
+}
+
+#[aoc_generator(day7)]
+pub fn input_generator(input: &str) -> Box<HashMap<char, Vec<char>>> {
+    let i = input.trim().lines().map(|l| {
+        let mut parts = l.split(' ');
+
+        (
+            parts.nth(1).unwrap().chars().next().unwrap(),
+            parts.nth(5).unwrap().chars().next().unwrap(),
+        )
+    });
+
+    let mut edges: HashMap<char, Vec<char>> = HashMap::new();
+    for (p, c) in i {
+        edges.entry(c).or_default().push(p);
+        edges.entry(p).or_default();
+    }
+
+    Box::new(edges)
 }
 
 fn next_steps(edges: &HashMap<char, Vec<char>>, visited: &HashSet<char>) -> Vec<char> {
@@ -29,15 +43,9 @@ fn next_steps(edges: &HashMap<char, Vec<char>>, visited: &HashSet<char>) -> Vec<
 }
 
 #[aoc(day7, part1)]
-fn answer_1(input: &[(char, char)]) -> String {
+fn answer_1(edges: &HashMap<char, Vec<char>>) -> String {
     let mut s = String::new();
-    let mut edges: HashMap<char, Vec<char>> = HashMap::new();
     let mut visited: HashSet<char> = HashSet::new();
-
-    for (p, c) in input {
-        edges.entry(*c).or_default().push(*p);
-        edges.entry(*p).or_default();
-    }
 
     loop {
         let mut next = next_steps(&edges, &visited);
@@ -51,6 +59,53 @@ fn answer_1(input: &[(char, char)]) -> String {
     }
 
     s
+}
+
+fn time_required(step: char) -> usize {
+    let step_delay = 60;
+    step_delay + (step as u8 - 'A' as u8) as usize
+}
+
+#[aoc(day7, part2)]
+fn answer_2(edges: &HashMap<char, Vec<char>>) -> usize {
+    let mut total_time = 0;
+    let n_workers = 5;
+    let mut workers: Vec<Work> = Vec::with_capacity(n_workers);
+
+    let mut visited: HashSet<char> = HashSet::new();
+
+    loop {
+        let mut new_workers = Vec::with_capacity(n_workers);
+        while let Some(w) = workers.pop() {
+            if w.is_finished(total_time) {
+                visited.insert(w.1);
+            } else {
+                new_workers.push(w);
+            }
+        }
+
+        let mut next = next_steps(&edges, &visited);
+        if next.len() == 0 {
+            break;
+        }
+
+        next.sort();
+        for step in next {
+            if new_workers.len() >= n_workers {
+                break;
+            }
+
+            if new_workers.iter().find(|w| w.1 == step).is_none() {
+                new_workers.push(Work(total_time, step));
+            }
+        }
+
+        workers = new_workers;
+        total_time += 1;
+        assert!(workers.len() <= n_workers);
+    }
+
+    total_time
 }
 
 #[cfg(test)]
@@ -70,4 +125,9 @@ Step F must be finished before step E can begin.\
     fn examples_1() {
         assert_eq!(answer_1(&input_generator(TEST_INPUT)), "CABDFE");
     }
+    #[test]
+    fn examples_2() {
+        assert_eq!(answer_2(&input_generator(TEST_INPUT)), 253);
+    }
+
 }
